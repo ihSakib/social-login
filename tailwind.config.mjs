a340ff5992e86da05510a3a09221341c6c2c9a3c
ihSakib <ihSakib@outlook.com>
2025-03-16 18:47:12 +0600

    Remove custom CSS variables for background and foreground colors from globals.css and update Tailwind CSS configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,13 +9,6 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  
   plugins: [],
 };
